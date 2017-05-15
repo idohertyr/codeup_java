ec45a5d1c060763fb261ccc64c77108fa756859e
Ian Doherty <idohertyr@gmail.com>
2017-05-15 01:11:12 -0500

    5.5 complete.

---
+++ Java1/StringExercises.java
@@ -0,0 +1,12 @@
+public class StringExercises {
+    public static void main(String[] args) {
+        String pre = "We don't need no ";
+        System.out.printf("%seducation.\n%sthought control.", pre, pre);
+
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+
+        System.out.println("In windows, the main drive is usually C:\\");
+
+        System.out.println("I can go backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!");
+    }
+}
